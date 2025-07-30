c310e1aba2f2afa05676e7c170404cae4850b1b9
tarek-code <tarekadel314@gmail.com>
2025-07-31 00:31:59 +0300

    fix: Optimize subtract function logic (introducing bug)

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def add(a,b):
     return a + b
 def subtract(a, b):
     #Returns the difference of a and b.
-    return a - b
+    return a + b # bug intentional
 def multiply(a, b):
     #Returns the product of a and b.
     return a * b
